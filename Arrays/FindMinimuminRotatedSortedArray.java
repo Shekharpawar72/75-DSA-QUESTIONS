5ccb6cb3879c45e406a5f9386212364e0bebed83
Shekharpawar72 <ShekharBarkahde7279@gmail.com>
2025-11-29 10:02:36 +0530

    Day-5 -revision Day

---
+++ Arrays/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class FindMinimuminRotatedSortedArray {
+    public  int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return nums[start];
+    }
+    public static void main(String[] args) {
+        FindMinimuminRotatedSortedArray solution = new FindMinimuminRotatedSortedArray();
+        int[] nums = {3,4,5,1,2};
+        System.out.println("Input: [3,4,5,1,2] -> Minimum Element: " + solution.findMin(nums));
+    }
+}
---
+++ Arrays/SearchinRotateSorteArray.java
@@ -0,0 +1,35 @@
+package Arrays;
+public class SearchinRotateSorteArray {
+    public int search( int nums[], int target){
+         int start =0;
+        int end = nums.length-1;
+
+        while(start<=end){
+             int mid = (start+end)/2;
+             if(nums[mid]== target){
+                 return mid;
+             }
+
+             if( nums[start]<=nums[mid]){
+                     if(nums[start]<=target && target <=nums[mid]){
+                         end= mid-1;
+                     }else{
+                         start = mid+1;
+                     }
+             }else{
+                 if(nums[mid]<=target && target<=nums[end]){
+                     start = mid+1;
+                 }else {
+                     end = mid-1;
+                 }
+             }
+        }
+           return -1;
+    }
+    public static void main(String[] args) {
+        SearchinRotateSorteArray solution = new SearchinRotateSorteArray();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println("Input: [4,5,6,7,0,1,2], target = 0 -> Output: " + solution.search(nums, target));
+    }
+}
