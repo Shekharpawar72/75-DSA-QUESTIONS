0e02645c7cc7b33790a5ed9b16a0a698f42466c4
Shekharpawar72 <ShekharBarkahde7279@gmail.com>
2025-12-05 09:49:01 +0530

    Day-10

--- Arrays/ValidAnagram.java
+++ Arrays/ValidAnagram.java
@@ -1,28 +1,45 @@
 package Arrays;
-
+ import java.util.Arrays;
 public class ValidAnagram {
      public boolean isAnagram(String s, String t) {
-        // Check if lengths are different
-        if (s.length() != t.length()) {
-            return false;
-        }
+    //     // Check if lengths are different
+    //     TIME COMPLEXITY : O(n) WHERE n IS THE LENGTH OF THE STRINGS
+    //     SPACE COMPLEXITY : O(1) SINCE THE COUNT ARRAY SIZE IS FIXED (26 FOR LOWERCASE ENGLISH LETTERS)
+    
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
 
-        int[] count = new int[26];
-        // Count frequency of each character in both strings
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++; // Increment count for character in s
-            count[t.charAt(i) - 'a']--; // Decrement count for character in t
-            // isme -- ku ki agar dono string same hai to count array me sab 0 ho jayega
-        }
+    //     int[] count = new int[26];
+    //     // Count frequency of each character in both strings
+    //     for (int i = 0; i < s.length(); i++) {
+    //         count[s.charAt(i) - 'a']++; // Increment count for character in s
+    //         count[t.charAt(i) - 'a']--; // Decrement count for character in t
+    //         // isme -- ku ki agar dono string same hai to count array me sab 0 ho jayega
+    //     }
 
-        for (int c : count) {
-            if (c != 0) {
-                return false;   // If any count is not zero, s and t are not anagrams
-            }
-        }
+    //     for (int c : count) {
+    //         if (c != 0) {
+    //             return false;   // If any count is not zero, s and t are not anagrams
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
+
+    // dusra approach sorting ka 
+    //TIME COMPLEXITY : O(n log n) DUE TO SORTING
+    //SPACE COMPLEXITY : O(1) IF SORTING IN PLACE IS USED, OTHERWISE O(n) IF ADDITIONAL SPACE IS USED FOR SORTED ARRAYS
+    char ssort [] = s.toCharArray();
+       char tsort []= t.toCharArray();
+
+
+       Arrays.sort(ssort);// ye dono sorted array ko compare karega
+       Arrays.sort(tsort); // ye dono sorted array ko compare karega
+
+         return Arrays.equals(ssort, tsort);
+
+     }
     public static void main(String[] args) {
         ValidAnagram solution = new ValidAnagram();
         String s = "anagram";
