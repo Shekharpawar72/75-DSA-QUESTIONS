ec8070ff0249b5fdd7b2ea496b41ced1a9a618c4
Shekharpawar72 <ShekharBarkahde7279@gmail.com>
2026-03-19 20:10:57 +0530

    tcs Question

---
+++ Arrays/zigzag.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class zigzag {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 7, 8, 6, 2, 1};
+        zigzag(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        
+    }
+
+    private static void zigzag(int[] arr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'zigzag'");
+    }
+}
