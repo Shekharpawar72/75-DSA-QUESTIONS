24840eae2028befd579ef6071e551e52c7f0b5a6
Shekharpawar72 <ShekharBarkahde7279@gmail.com>
2026-03-20 10:42:44 +0530

    tcs Nqt Questions

---
+++ Arrays/PrimeNumber.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         System.out.println("Enter Number ");
+        int num = sc.nextInt();
+       
+
+        boolean isprime = true;
+
+        if(num <=1){
+             isprime = false;
+        }else{
+            for(int i=2; i * i<=num; i++){  // we can also write i<=Math.sqrt(num) and it will work fine but i*i<=num is more efficient than i<=Math.sqrt(num) because it does not require the calculation of the square root.
+                 if(num % i == 0){
+                        isprime = false;
+                        break;
+                 }
+            }
+        }
+
+        if(isprime){
+                 System.out.println(num + " number is Prime number");
+        }else{
+             System.out.println(num + " Number is not Prime ");
+        }
+        sc.close();
+    }
+}
